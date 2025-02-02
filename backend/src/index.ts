622c0adeb382ee25311ebc787f21ceb7c2914a17
Michał Łowkiet <michallowkiet@gmail.com>
2025-02-02 11:45:20 +0100

    add route for static files

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -28,6 +28,10 @@ app.use('/api/message', protectedRoute, messageRoutes);
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../../frontend/dist')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../frontend/dist/index.html'));
+  });
 }
 
 // Start the server
