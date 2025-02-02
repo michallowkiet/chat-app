85d588007a5a3feb968b40c889db25c940928d60
Michał Łowkiet <michallowkiet@gmail.com>
2025-02-02 09:40:45 +0100

    add env cors origin to socket.io config

--- src/lib/socketio.ts
+++ src/lib/socketio.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import express from 'express';
 import http from 'http';
 import { Server } from 'socket.io';
@@ -7,7 +8,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ['http://localhost:5173'],
+    origin: [`${process.env.CORS_ORIGIN}`],
   },
 });
 
